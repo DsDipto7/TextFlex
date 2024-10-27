f76f7daf27ef434312b9ff689cb121daa81e93b9
dipto <diptosaha.ndc50@gmail.com>
2024-10-27 23:46:00 +0600

    Initial item added

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TexUtils" aboutText="About Text" />
+      <div className="container" mb-3>
+        <Textform heading="Enter Your Text To Analyze" />
+        <About/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Textform.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+export default function Textform(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    console.log("Uppercase has clicked");
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    console.log("Lowercase has clicked");
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    let newText = '';
+     setText(newText);
+  };
+
+
+  const handleSpeak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("On Change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter Text Here");
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="my-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="10"
+          ></textarea>
+        </div>
+
+
+        <button className="btn btn-success mx-1" onClick={handleUpClick}>
+          Convert To Uppercase
+        </button>
+
+
+        <button className="btn btn-info mx-2" onClick={handleLoClick}>
+          Convert To Lowercase
+        </button>
+
+
+        <button className="btn  mx-2" style={{ backgroundColor: '#9B7EBD', color: 'white' }} onClick={handleClearClick}>
+          Clear
+        </button>
+
+        {/* <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button> */}
+
+        <button className="btn mx-2" style={{ backgroundColor: '#98DED9', color: 'white' }} onClick={handleSpeak} >speak</button>
+
+
+
+      </div>
+      <div className="container" my-3>
+        <h1>Your Text Summary </h1>
+        <p>
+          <p>
+            {text.trim().split(/\s+/).length} Words and {text.length} Characters
+          </p>
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes take to read it</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
